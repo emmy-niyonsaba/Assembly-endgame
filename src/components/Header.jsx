c75c2271b9bf9e058054ba7257c405a35a79dcb2
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-20 16:09:01 +0200

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,15 @@ function Header() {
         <h1 className="text-4xl md:text-5xl font-extrabold tracking-wide">
           Assembly: <span className="text-blue-500">Endgame</span>
         </h1>
+        <h1 className="text-4xl md:text-5xl font-extrabold tracking-wide">
+          By Emmanuel Niyonsaba
+        </h1>
 
         <p className="mt-6 text-lg md:text-xl text-gray-300 leading-relaxed max-w-2xl mx-auto">
           Guess the word within 
           <span className="font-semibold text-white"> 8 attempts </span>
           to keep the programming world safe from 
-          <span className="text-red-400 font-semibold"> Assembly</span>!
+          <span className="text-red-400 font-semibold"> Assembly </span>!
         </p>
 
         <div className="mt-8 w-20 h-1 bg-blue-500 mx-auto rounded-full"></div>
