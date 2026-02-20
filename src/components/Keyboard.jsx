f9940f00e0774d2035bd7873ed653f4c55149ce0
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-20 10:15:10 +0200

    add the new game button

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,14 +1,17 @@
 function Keyboard (){
     const alphabets="abcdefghijklmnopqrstuvwxyz".split("");
     return (
+        <>
         <div  className=" flex flex-wrap justify-center gap-2 w-130 mx-auto ">
             {alphabets.map((alphabet) => (
-                <button key={alphabet} className=" w-10 h-10 border-2 border-be-black cursor-pointer bg-amber-300 text-lg font-bold">
+                <button key={alphabet} className=" w-10 h-10 border-2 border-be-black cursor-pointer bg-amber-300 text-lg font-bold rounded-md">
                     {alphabet.toUpperCase()}
                 </button>
             ))}
 
         </div>
+        <button className=" bg-blue-400 p-2 rounded-md flex m-5 mx-auto  items-center">New Game</button>
+            </>
 
       )              
 }
--- src/components/WordDisplyer.jsx
+++ src/components/WordDisplyer.jsx
@@ -7,7 +7,7 @@ function WordDisplyer() {
     <div className='w-120 mx-auto flex justify-center gap-2 m-4'>
       {
         currentWord.split('').map((letter, index) => (
-            <span key={index} className="text-2xl font-bold mx-1 border-2 border-gray-300 rounded p-2 bg-gray-500"> 
+            <span key={index} className="text-2xl font-bold mx-1 border-2 border-gray-300 rounded-md p-2 bg-gray-500"> 
                 {letter.toUpperCase()}
             </span>
         ))
